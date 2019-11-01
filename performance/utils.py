96c2c06510a2bc1a42509ab28fb1e7fc9237aede
msramalho <up201403027@fe.up.pt>
2019-11-01 12:49:48 +0000

    isolated some useful functions in a utils.py file

---
+++ performance/utils.py
@@ -0,0 +1,15 @@
+import subprocess, os, shutil, sys
+
+
+def custom_subprocess(params, silent=False, done_message=None):
+    if silent:
+        return_code = subprocess.call(params, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+    else:
+        return_code = subprocess.call(params)
+
+    if done_message and return_code == 0: print(done_message)
+    return return_code
+
+
+def make_project(silent=True):
+    custom_subprocess(["make", "-C", "../code/"], "Compilation successfull")
